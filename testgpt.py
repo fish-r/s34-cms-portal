a97f201e33e56b67ecd52368a3c301f81b41fef6
Ðéмø ✞ <jiasennn@gmail.com>
2023-11-08 19:13:30 +0800

    kolb testing

---
+++ testgpt.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+import transformers
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+from llama_index import Document, VectorStoreIndex, SimpleDirectoryReader
+import openai
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+# Load the hugging face model
+openai.api_key = os.environ["OPENAI_API_KEY"]
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased")
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+# Create a Document object for each text file in the directory
+documents = SimpleDirectoryReader('./testdata').load_data()
+
+
+# Create a GPTVectorStoreIndex object from a list of Document objects
+index = VectorStoreIndex.from_documents(documents)
+
+# Index the documents
+index.index()
+
+
+# Query the index
+query = "What is the capital of France?"
+predictions = index.query(query)
+
+# Print the predictions
+for prediction in predictions:
+    print(prediction)
